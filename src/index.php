5534f7d486ba04920dc226f8855cae6994aa4e8f
Adam Zeloof <azzeloof@users.noreply.github.com>
2025-01-01 23:18:41 -0500

    It actually looks like a website now

--- src/index.php
+++ src/index.php
@@ -1,6 +1,33 @@
 <?php $title = 'Home'; include("top.php");?>
 
-<h1>Hello, world!</h1>
-aaaaaaaaaaaaaaaa
+<div class="cover-container d-flex w-100 h-100 p-3 mx-auto flex-column">
+  <header class="mb-auto">
+    <!--
+    <div>
+    <h3 class="float-md-start mb-0">Cover</h3>
+      <nav class="nav nav-masthead justify-content-center float-md-end">
+        <a class="nav-link fw-bold py-1 px-0 active" aria-current="page" href="#">Home</a>
+        <a class="nav-link fw-bold py-1 px-0" href="#">Features</a>
+        <a class="nav-link fw-bold py-1 px-0" href="#">Contact</a>
+      </nav>
+    </div>
+-->
+  </header>
+  
+
+  <main class="px-2">
+    <h1>Sara & Adam Are Getting Married</h1>
+    <p class="lead">Sometime and someplace. More details to come once we figure them out.</p>
+    <!--
+    <p class="lead">
+      <a href="#" class="btn btn-lg btn-light fw-bold border-white bg-white">Learn more</a>
+    </p>
+    -->
+  </main>
+
+  <footer class="mt-auto text-white-50">
+    <p>Copyright 2025, Adam Zeloof and Sara Adkins • <a href="https://github.com/azzeloof/wedsite" target="blank" class="text-white"><i class="bi bi-github"></i></a></p>
+  </footer>
+</div>
 
 <?php include("bottom.php");?>
