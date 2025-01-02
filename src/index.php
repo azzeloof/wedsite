7694376e7341506f0639eae0c299a83c7e34d5da
Adam Zeloof <azzeloof@gmail.com>
2025-01-02 14:35:49 -0500

    adding pages

--- src/index.php
+++ src/index.php
@@ -1,17 +1,18 @@
 <?php $title = 'Home'; include("top.php");?>
 
-<div class="cover-container d-flex w-100 h-100 p-3 mx-auto flex-column">
+<div class="cover-container d-flex w-100 h-100 py-3 px-4 mx-auto flex-column">
   <header class="mb-auto">
-    <!--
     <div>
-    <h3 class="float-md-start mb-0">Cover</h3>
+    <!--<h3 class="float-md-start mb-0">Cover</h3>-->
       <nav class="nav nav-masthead justify-content-center float-md-end">
-        <a class="nav-link fw-bold py-1 px-0 active" aria-current="page" href="#">Home</a>
-        <a class="nav-link fw-bold py-1 px-0" href="#">Features</a>
-        <a class="nav-link fw-bold py-1 px-0" href="#">Contact</a>
+        <a class="nav-link fw-bold py-1 px-0" href="#">Who</a>
+        <a class="nav-link fw-bold py-1 px-0 active" aria-current="page" href="#">What</a>
+        <a class="nav-link fw-bold py-1 px-0" href="#">When</a>
+        <a class="nav-link fw-bold py-1 px-0" href="#">Where</a>
+        <a class="nav-link fw-bold py-1 px-0" href="#">Why</a>
+        <a class="nav-link fw-bold py-1 px-0" href="#">How</a>
       </nav>
     </div>
--->
   </header>
   
 
