628ad336a7686f3c538a4ca209193162c7bb154f
Adam Zeloof <azzeloof@users.noreply.github.com>
2025-01-02 15:10:04 -0500

    Update index.php

--- src/index.php
+++ src/index.php
@@ -1,10 +1,11 @@
 <?php $title = 'Home'; include("top.php");?>
 
 <div class="cover-container d-flex w-100 h-100 py-3 px-4 mx-auto flex-column">
-  <!--
+
   <header class="mb-auto">
+      <!--
     <div>
-    <!--<h3 class="float-md-start mb-0">Cover</h3>-->
+   <h3 class="float-md-start mb-0">Cover</h3>
       <nav class="nav nav-masthead justify-content-center float-md-end">
         <a class="nav-link fw-bold py-1 px-0" href="#">Who</a>
         <a class="nav-link fw-bold py-1 px-0 active" aria-current="page" href="#">What</a>
@@ -14,8 +15,9 @@
         <a class="nav-link fw-bold py-1 px-0" href="#">How</a>
       </nav>
     </div>
+
+  -->
   </header>
--->
   
 
   <main class="px-2">
