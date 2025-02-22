cceb709712dac851d2b08a7375b61b9ad5d4780c
Adam Zeloof <azzeloof@gmail.com>
2025-02-21 23:07:57 -0500

    updated photos

--- src/party_profiles/06-ashley.php
+++ src/party_profiles/06-ashley.php
@@ -6,7 +6,7 @@ $profile = new Profile();
 $profile->name = 'Ashley Wong';
 $profile->type = 'Creature - Human Calculator';
 $profile->cost = '<i class="ms ms-r ms-cost ms-shadow"></i><i class="ms ms-r ms-cost ms-shadow"></i>';
-$profile->image = '/';
+$profile->image = '/party_profiles/images/ashley.jpg';
 $profile->description = 'words';
 $profile->quote = '';
 $profile->powerToughness= "1/1";
--- src/party_profiles/09-shahar.php
+++ src/party_profiles/09-shahar.php
@@ -6,7 +6,7 @@ $profile = new Profile();
 $profile->name = 'Shahar Elisha';
 $profile->type = 'Creature - Audio Scientist';
 $profile->cost = '<i class="ms ms-w ms-cost ms-shadow"></i><i class="ms ms-w ms-cost ms-shadow"></i>';
-$profile->image = '/party_profiles/images/';
+$profile->image = '/party_profiles/images/shahar.jpg';
 $profile->description = 'words';
 $profile->quote = '';
 $profile->powerToughness= "1/1";
