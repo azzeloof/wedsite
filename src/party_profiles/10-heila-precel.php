2222dbc34ebf649f20572c8e42d98d67e72a78da
Adam Zeloof <azzeloof@gmail.com>
2025-02-22 21:31:06 -0500

    profile updates

--- src/party_profiles/10-heila-precel.php
+++ src/party_profiles/10-heila-precel.php
@@ -5,7 +5,7 @@ include_once('./classes/profile.php');
 $profile = new Profile();
 $profile->name = 'Heila Precel';
 $profile->type = 'Creature - AI Ethics Scientist';
-$profile->cost = '<i class="ms ms-c ms-cost ms-shadow"></i><i class="ms ms-c ms-cost ms-shadow"></i>';
+$profile->cost = '<i class="ms ms-g ms-cost ms-shadow"></i><i class="ms ms-w ms-cost ms-shadow"></i>';
 $profile->image = '/party_profiles/images/heila.jpg';
 $profile->description = "Heila met Adam and Sara freshman year of college and then 5 years later they became friends. She is talented at debating pointless topics such as the cube rule and coffee vs soda. Heila will be officiating the wedding and is our only friend good at public speaking";
 $profile->quote = 'Worst case, I will wear stilts.';
