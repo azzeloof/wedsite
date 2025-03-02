b35d39804af5e82db9600c206cdc3b0be0c94535
Adam Zeloof <azzeloof@users.noreply.github.com>
2025-03-01 22:15:05 -0500

    Added a map

--- src/where.php
+++ src/where.php
@@ -3,11 +3,23 @@
 
 <body class="d-flex h-100 text-bg-light">
     <div class="d-flex w-100 h-100 py-3 px-4 mx-auto flex-column">
-    <?php include("menu.php");?>
-    <div class="container my-5">
-    <h1>Sergeantsville, NJ</h1>
-    <p>Address will be printed on the Invitation.</p>
-    <p>Lodging recommendations will be added here.</p>
-    </div>
+        <?php include("menu.php");?>
+        <div class="container my-5">
+            <div class="row">
+                <div class="row py-3">
+                    <h1>Sergeantsville, NJ</h1>
+                </div>
+                <div class="col-md-4">
+                    <div class="row my-3">
+                        <p>The exact address will be printed on your invitation.</p>
+                        <p>The closest major airports are Philadelphia and Newark (Sergeantsville is about an hour drive from each). The closest Amtrak station is in Trenton, about a half hour away. There will be plenty of parking available.</p>
+                        <p>Local lodging recommendations will be added here as we get closer to the date. If you need to plan your travel sooner just ask Adam or Sara!</p>
+                    </div>
+                </div>
+                <div class="col-md-8">
+                    <img src="/images/map.png" width="100%"></img>
+                </div>
+            </div>
+        </div>
 
 <?php include("bottom.php");?>
