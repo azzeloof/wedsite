5e818ad19152ffdcdd1abff3e99e43403b0b96e6
Adam Zeloof <azzeloof@users.noreply.github.com>
2025-03-01 22:23:54 -0500

    travel page updates

--- src/where.php
+++ src/where.php
@@ -12,7 +12,7 @@
                 <div class="col-md-4">
                     <div class="row my-3">
                         <p>The exact address will be printed on your invitation.</p>
-                        <p>The closest major airports are Philadelphia and Newark (Sergeantsville is about an hour drive from each). The closest Amtrak station is in Trenton, about a half hour away. There will be plenty of parking available.</p>
+                        <p>The closest major airports are Philadelphia and Newark (Sergeantsville is about an hour drive from each). For international travelers, JFK is also a good option. The closest Amtrak station is in Trenton, about a half hour away. From New York and Newark, you can grab an NJ Transit train to Raritan Station, which is also about a half hour drive away. There will be plenty of parking available.</p>
                         <p>Local lodging recommendations will be added here as we get closer to the date. If you need to plan your travel sooner just ask Adam or Sara!</p>
                     </div>
                 </div>
