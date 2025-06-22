62260fec1af6b35429676434af5ad1d8616ae423
Satrat <sara.adkins65@gmail.com>
2025-06-22 18:13:17 -0400

    schedule, dress code, gifts

--- src/when.php
+++ src/when.php
@@ -5,7 +5,16 @@
     <div class="d-flex w-100 h-100 py-3 px-4 mx-auto flex-column">
     <?php include("menu.php");?>
     <div class="container my-5">
-    <h1>Saturday October 11th, 2025 at 3 PM</h1>
+    <h1>Saturday October 11th, 2025</h1>
+    <h2>3pm: Ceremony</h2>
+    <p>Outdoors by the pond</p>
+    <h2> 3:30pm: Cocktail Hour</h2>
+    <p>Hors d'oeuvres, drinks and lawn games on the back patio</p>
+    <h2>5pm: Reception</h2>
+    <p>Dinner buffet, drinks and live music in the barn</p>
+    <h2> 10:30pm: Late Night Snack</h2>
+    <p>Food truck and closing DJ set in the barn</p>
+    <h1>Sunday October 12th, 2025</h1>
     <p>If you're able to stick around on Sunday (Monday is a holiday), we'll be hanging out at the farm all day for breakfast, pumpkin carving, and a Barn Jam (concert) in the afternoon.</p>
     <p>Exact schedule to come later.</p>
     </div>
