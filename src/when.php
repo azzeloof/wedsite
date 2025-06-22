a1a7c616cc0696e13603aa5f9a1a18478014b9ea
Adam Zeloof <azzeloof@users.noreply.github.com>
2025-06-22 09:39:18 -0400

    Update when.php

--- src/when.php
+++ src/when.php
@@ -5,8 +5,9 @@
     <div class="d-flex w-100 h-100 py-3 px-4 mx-auto flex-column">
     <?php include("menu.php");?>
     <div class="container my-5">
-    <h1>Saturday October 11th, 2025</h1>
-    <p>Schedule to come later.</p>
+    <h1>Saturday October 11th, 2025 at 3 PM</h1>
+    <p>If you're able to stick around on Sunday (Monday is a holiday), we'll be hanging out at the farm all day for breakfast, pumpkin carving, and a Barn Jam (concert) in the afternoon.</p>
+    <p>Exact schedule to come later.</p>
     </div>
 
 <?php include("bottom.php");?>
