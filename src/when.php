3554ed77a81ae7df05ab16f98c206ed5fffa754b
Adam Zeloof <azzeloof@gmail.com>
2025-06-23 18:43:33 -0400

    corrected error messages on login form

--- src/when.php
+++ src/when.php
@@ -5,19 +5,24 @@
     <div class="d-flex w-100 h-100 py-3 px-4 mx-auto flex-column">
     <?php include("menu.php");?>
     <div class="container my-5">
-    <h1>Saturday October 11th, 2025</h1>
-    <p>Please aim to arrive at 2:30 for a prompt ceremony start time!</p>
-    <h3>3:00 pm: Ceremony</h3>
-    <p>Outdoors by the pond</p>
-    <h3>3:30 pm: Cocktail Hour</h3>
-    <p>Hors d'oeuvres, drinks and lawn games on the back patio</p>
-    <h3>5:00 pm: Reception</h3>
-    <p>Dinner buffet, live music and dancing in the barn</p>
-    <h3>10:30 pm: Late Night Snack</h3>
-    <p>Food truck and closing DJ set in the barn</p>
-    <h1>Sunday October 12th, 2025</h1>
-    <p>If you're able to stick around on Sunday (Monday is a holiday), we'll be hanging out at the farm all day for breakfast, pumpkin carving, and a Barn Jam (concert) in the afternoon.</p>
-    <p>Exact schedule to come later.</p>
+        <div class="col-md-6">
+            <h1>Saturday October 11th, 2025</h1>
+            <p>Please aim to arrive at 2:30 for a prompt ceremony start time!</p>
+            <h3>3:00 pm: Ceremony</h3>
+            <p>Outdoors by the pond</p>
+            <h3>3:30 pm: Cocktail Hour</h3>
+            <p>Hors d'oeuvres, drinks and lawn games on the back patio</p>
+            <h3>5:00 pm: Reception</h3>
+            <p>Dinner buffet, live music and dancing in the barn</p>
+            <h3>10:30 pm: Late Night Snack</h3>
+            <p>Food truck and closing DJ set in the barn</p>
+            <h1>Sunday October 12th, 2025</h1>
+            <p>If you're able to stick around on Sunday (Monday is a holiday), we'll be hanging out at the farm all day for breakfast, pumpkin carving, and a Barn Jam (concert) in the afternoon.</p>
+            <p>Exact schedule to come later.</p>
+        </div>
+        <div class="col-md-6">
+            <img src="/images/tree.jpeg" width="100%"></img>
+        </div>
     </div>
 
 <?php include("bottom.php");?>
