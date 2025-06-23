6d4942355add8f3d155c63bb9876d0fcae4569c9
Satrat <sara.adkins65@gmail.com>
2025-06-22 20:29:35 -0400

    reformat

--- src/how.php
+++ src/how.php
@@ -6,9 +6,9 @@
     <?php include("menu.php");?>
     <div class="container my-5">
     <h1>Dress Code</h1>
-    <p>Dress code is semi-formal/cocktail attire. You'll be walking on grass to get around the property so we recommend comfortable footwear! The ceremony and cocktail hour will be outdoors (weather permitting) with heaters, and the reception will be inside the barn. </p>
+    <p>Dress code is semi-formal/cocktail attire. You'll be walking on grass to get around the property so we recommend comfortable footwear! The ceremony and cocktail hour will be outdoors (weather permitting) with heaters, and the reception will be inside the barn.</p>
     <h1>Gifts</h1>
-    <p>As we've been living together for several years, we aren't in need of any traditional wedding registry items (Adam already owns the best version of every kitchen tool imaginable anyways). Spending time with our favorite people is all the gift we need, but if you'd like to give something we would love a handmade item that shows off your craft, a personal card or a family recipe. </p>
+    <p>As we've been living together for several years, we aren't in need of any traditional wedding registry items (Adam already owns the best version of every kitchen tool imaginable anyways). Spending time with our favorite people is all the gift we need, but if you'd like to give something we would love a handmade item that shows off your craft, a personal card or a family recipe.</p>
     </div>
 
 <?php include("bottom.php");?>
