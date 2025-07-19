38af26858bc03597ef6801a14b5537d3ceba6168
Adam Zeloof <azzeloof@gmail.com>
2025-07-19 01:14:21 -0400

    reverted a lot of things and reimplemented validation

--- src/guest_portal.php
+++ src/guest_portal.php
@@ -198,4 +198,26 @@ if (isset($_SESSION['rsvp_error_message'])) {
                     </fieldset>
                 </form>
                     </div></div>
+        <script>
+// Example starter JavaScript for disabling form submissions if there are invalid fields
+(() => {
+  'use strict'
+
+  // Fetch all the forms we want to apply custom Bootstrap validation styles to
+  const forms = document.querySelectorAll('.needs-validation')
+
+  // Loop over them and prevent submission
+  Array.from(forms).forEach(form => {
+    form.addEventListener('submit', event => {
+      if (!form.checkValidity()) {
+        event.preventDefault()
+        event.stopPropagation()
+      }
+
+      form.classList.add('was-validated')
+    }, false)
+  })
+})()
+</script>
+
         <?php include("bottom.php");?>
